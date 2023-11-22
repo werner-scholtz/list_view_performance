8f5229a5157b0cb1a3d5cbc79af70b2fae0ec080
werner-scholtz <werner.scholtz@kdab.com>
2023-11-22 12:47:12 +0200

    Readme changes

--- integration_test/data_tile_test.dart
+++ integration_test/data_tile_test.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
 import 'package:list_view_performance/main.dart';
 
 import '../test_driver/perf_driver.dart';
 import 'constants.dart';
 import 'utils.dart';
 
-/// flutter drive --driver=test_driver/perf_driver.dart --target=integration_test/custom_list_view_test.dart -d Linux --profile
+/// flutter drive --driver=test_driver/perf_driver.dart --target=integration_test/data_tile_test.dart -d Linux --profile
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
-
-  testWidgets('Performance Test for CustomDataTile', (tester) async {
+  testWidgets('Performance Test for DataTile', (tester) async {
     await tester.pumpWidget(MyApp(items: items));
     await tester.pumpAndSettle();
-    await tester.tap(find.byKey(const ValueKey('custom_data_tile_list_view')));
+    await tester.tap(find.byKey(const ValueKey('data_tile_list_view')));
+    await tester.pumpAndSettle();
     await tester.pumpAndSettle(const Duration(milliseconds: 250));
 
     final firstItemFinder = find.byKey(firstItemKey);
@@ -33,7 +32,7 @@ void main() {
           delta: delta,
         );
       },
-      reportKey: customDataTileReportKey,
+      reportKey: dataTileReportKey,
     );
   });
 }
